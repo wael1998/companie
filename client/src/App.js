c43494ad97e63c640e4bcef9060495ea7ff0aa26
Hrairi Mehdi <49190228+MEHDIHRAIRI@users.noreply.github.com>
2021-12-07 15:29:17 +0100

    clean react init projet and prepare main routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Fragment>
+          <Routes>
+            <Route path="/todo" component={Todo} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+          </Routes>
+        </Fragment>
+      </Router>
+    </>
   );
 }
 
